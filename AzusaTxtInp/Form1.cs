f9ddc9f7c121b964288729c85d32940d006f6108
Uchuu Tamashi <ytc@mit.edu>
2014-08-29 16:53:20 +0800

    init

---
+++ AzusaTxtInp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AzusaTxtInp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //configurate global hotkeys
+            int id = 0;     // The id of shift space 
+            RegisterHotKey(this.Handle, id, (int)KeyModifier.Shift, Keys.Space.GetHashCode());   
+       
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Shared.msg = textBox1.Text;
+                textBox1.Text = "";
+                this.Hide();
+
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                textBox1.Text = "";
+                this.Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+    }
+}
---
+++ AzusaTxtInp/Shared.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AzusaTxtInp
+{
+    static class Shared
+    {
+        static public string msg = "";
+        
+    }
+}
